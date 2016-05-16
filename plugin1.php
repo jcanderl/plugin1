dbe432776f9722c0c1bc5b638adafc333c03f832
JC <jc@profitpeakmarketing.com>
2016-05-16 06:41:38 -0500

    Create plugin file

---
+++ plugin1.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: JCA
+Plugin URI: https://profitpeakmarketing.com
+Description: Test Plugin
+Version: 1.0
+Author: JC Anderl
+Author URI: https://profitpeakmarketing.com
+License: GPL2
+*/
